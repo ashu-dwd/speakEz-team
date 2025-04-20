b65260d228bb81e1981c1e74be9cb6c0b67a742b
dwivediji425 <dwivediji425@gmail.com>
2025-04-20 17:04:43 +0530

    Add chat and AI character routes, controllers, and models; integrate multer for file uploads

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import userRoute from './Routes/userRoute.js';
 import userDataRoute from './Routes/userDataRoute.js';
+import chatRoute from './Routes/chatRoute.js';
+import aiCharRoute from './Routes/aiCharRoute.js';
 import connectDB from "./connect.js";
 import verifyToken from "./Middlewares/auth.js";
 
@@ -21,6 +23,8 @@ app.use(express.json());
 //middleware for routes
 app.use('/api/user', userRoute);
 app.use('/api/userData', verifyToken, userDataRoute);
+app.use('/api/chat', verifyToken, chatRoute);
+app.use('/api/aiChar', aiCharRoute)
 
 
 app.listen(PORT, () => {
